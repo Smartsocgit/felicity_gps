b44d5bfdb871e0978ae5cf901d2b1558d7390a01
Smartsocgit <satya.kondeti@smartsocs.com>
2024-12-24 10:29:46 +0530

    Create gps_loc_maker.sh

---
+++ gps_loc_maker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the path to the shell script
+
+# Create the 'scripts' directory if it doesn't exist
+mkdir -p /opt/aikaan/scripts
+
+SHELL_SCRIPT_PATH="/opt/aikaan/scripts/gps-location.sh"
+
+# Create the shell script with the initial content
+cat << EOF > $SHELL_SCRIPT_PATH
+#!/bin/sh
+echo lat=26.002258133942785
+echo lon=92.8513598078571
+EOF
+
+# Make the shell script executable and give read and write permissions
+chmod 766 $SHELL_SCRIPT_PATH
+
+# Confirm the script has been created and made executable
+echo "Shell script created and set as executable and readable/writable at $SHELL_SCRIPT_PATH"
