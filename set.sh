e66b5ca3513417ba7b9376ead7b8fce9f9e95aa4
Smartsocgit <satya.kondeti@smartsocs.com>
2025-01-08 16:56:13 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -14,14 +14,18 @@ echo "Installing python3-pip and pyserial..."
 sudo apt install -y python3-pip
 pip3 install pyserial
 
-# Step 2: Make sure gps_loc_maker.sh is executable and execute it
+# Step 2: Set permissions for USB ports
+echo "Setting permissions for all USB ports..."
+sudo chmod 666 /dev/ttyUSB*
+
+# Step 3: Make sure gps_loc_maker.sh is executable and execute it
 echo "Setting execute permissions for gps_loc_maker.sh..."
 chmod +x "${GPS_LOCATION_MAKER}"
 
 echo "Executing gps_loc_maker.sh..."
 sudo "${GPS_LOCATION_MAKER}"
 
-# Step 3: Create the systemd service file
+# Step 4: Create the systemd service file
 echo "Creating systemd service file..."
 sudo bash -c "cat << EOF > ${SERVICE_FILE}
 [Unit]
@@ -39,11 +43,11 @@ WorkingDirectory=${DIRECTORY}
 WantedBy=multi-user.target
 EOF"
 
-# Step 4: Set proper permissions for the service file
+# Step 5: Set proper permissions for the service file
 echo "Setting permissions for the service file..."
 sudo chmod 644 "${SERVICE_FILE}"
 
-# Step 5: Enable and start the service
+# Step 6: Enable and start the service
 echo "Enabling and starting the service..."
 sudo systemctl daemon-reload
 sudo systemctl enable "${SERVICE_NAME}"
